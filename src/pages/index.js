4597806f6fdba2ccbce4aebdac98916de2243c9b
Jayakrishna <katikamjayakrishna143@gmail.com>
2023-07-24 20:25:05 +0530

    static web site with gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from "react"
-import { graphql, Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from "../components/Layout"
-import styles from '../styles/home.module.css'
+import * as styles from '../styles/home.module.css'
+import Img from "gatsby-image"
 
 export default function Home({ data }) {
   console.log(data)
-  const { title, description } = data.site.siteMetadata
+
   return (
     <Layout>
       <section className={styles?.header}>
@@ -13,21 +14,21 @@ export default function Home({ data }) {
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Manchester.</p>
-          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
+          <Link className={styles?.btn} to="/projects">My Portfolio Projects</Link>
         </div>
-        <img src="/banner.png" alt="site banner" style={{ maxWidth: '100%' }} />
-        <p>{ title } - { description }</p>
+        <Img fluid={data.file.childImageSharp.fluid} />
       </section>
     </Layout>
   )
 }
 
 export const query = graphql`
-  query SiteInfo {
-    site {
-      siteMetadata {
-        description
-        title
+  query Banner {
+    file(relativePath: {eq: "banner.png"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
       }
     }
   }
